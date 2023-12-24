3aa582afb928f2ae9926fc972e50ab547adf5e85
rishabhrahulcom <email@rishabhrahul.com>
2023-12-24 23:49:19 +0530

    Init

---
+++ src/main/java/com/rishabhrahul/influencer/InfluencerApplication.java
@@ -0,0 +1,13 @@
+package com.rishabhrahul.influencer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluencerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfluencerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rishabhrahul/influencer/InfluencerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishabhrahul.influencer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluencerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
